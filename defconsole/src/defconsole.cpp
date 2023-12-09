827fa6713688f675f3172aae35550077d47de7dc
Dmitry Senyushkin <dsenyushkin.dev@gmail.com>
2023-12-09 20:59:03 +0300

    Update defconsole.cpp

--- defconsole/src/defconsole.cpp
+++ defconsole/src/defconsole.cpp
@@ -4,6 +4,38 @@
 // include the Defold SDK
 #include <dmsdk/sdk.h>
 
+static int Reverse(lua_State* L)
+{
+    // The number of expected items to be on the Lua stack
+    // once this struct goes out of scope
+    DM_LUA_STACK_CHECK(L, 1);
+
+    // Check and get parameter string from stack
+    char* str = (char*)luaL_checkstring(L, 1);
+
+    // Reverse the string
+    int len = strlen(str);
+    for(int i = 0; i < len / 2; i++) {
+        const char a = str[i];
+        const char b = str[len - i - 1];
+        str[i] = b;
+        str[len - i - 1] = a;
+    }
+
+    // Put the reverse string on the stack
+    lua_pushstring(L, str);
+
+    // Return 1 item
+    return 1;
+}
+
+// Functions exposed to Lua
+static const luaL_reg Module_methods[] =
+{
+    {"reverse", Reverse},
+    {0, 0}
+};
+
 static void LuaInit(lua_State* L)
 {
     int top = lua_gettop(L);
